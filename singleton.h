28bd47f7e4d7ad6e69da93da0e0b46defb11c3c6
djet_00 <mishadruzhickii007@mail.ru>
2021-04-08 00:01:44 +0800

    Описал класс Enterprise

--- singleton.h
+++ singleton.h
@@ -1,4 +1,67 @@
-#ifndef SINGLETON_H
-#define SINGLETON_H
+#include <QList>
+#include <QString>
+#include <QDebug>
+
+class Enterprise {
+private:
+
+    QString companyName_;//название
+    double income_;//доход
+    double square_;// площадь
+    int numberOfWorkers_;//кол-во сотрудников
+    QList<QString> owners;
+
+public:
+
+    enum class typeEnterprise {
+                                  SINGLE,//Единичное
+                                  SERIAL,//Серийное
+                                  MASS,//Массовое
+                                };
+    Enterprise()
+    {
+        companyName_ = "0";
+        income_ = 0;
+        numberOfWorkers_ = 0;
+        square_ = 0;
+    }
+Enterprise(QString companyName, double income, double square, int numberOfWorkers,QList<QString> lList)	//конструктор, по умолчанию не имеет смысла(наверно)
+{
+
+    companyName_ = companyName;
+    income_ = income;
+    numberOfWorkers_ = numberOfWorkers;
+    square_ = square;
+    owners = lList;
+}
+~Enterprise()
+{
+
+}
+
+int getNumberOfWorkers()
+{
+    return numberOfWorkers_;
+}
+double getSquare()
+{
+    return square_;
+}
+double getIncome()
+{
+    return income_;
+}
+QString getNameCompany()
+{
+   return companyName_;
+}
+
+QList<QString> getListOwners()
+{
+    return owners;
+}
+virtual float getTax() = 0;//получение налога, реализация под каждый тип будет своя
+virtual typeEnterprise getType()  = 0;//получение типа предприятия
+};
 
 #endif // SINGLETON_H
