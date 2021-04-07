23a61d820d170cee6e80a8bc2596ab4cbb70044d
djet_00 <mishadruzhickii007@mail.ru>
2021-04-08 00:03:05 +0800

    Реализовал дочерние классы

--- singleton.h
+++ singleton.h
@@ -65,3 +65,106 @@ virtual typeEnterprise getType()  = 0;//получение типа предпр
 };
 
 #endif // SINGLETON_H
+class Single :public Enterprise
+{
+private:
+typeEnterprise type =typeEnterprise::SINGLE;
+float tax;
+
+
+public:
+Single():Enterprise()
+{
+
+}
+Single(QString companyName, double income, double square, int numberOfWorkers,QList<QString> lList)
+    :Enterprise(companyName, income, square, numberOfWorkers,lList)
+{
+
+}
+Enterprise::typeEnterprise getType() override //вернуть тип enum
+{
+    switch(type)
+    {
+        case typeEnterprise::SINGLE  : qDebug() << "Это предприятие имеет тип: SINGLE\n";  return type;  break;
+        case typeEnterprise::SERIAL  : qDebug() << "Это предприятие имеет тип: SERIAL\n";  return type; break;
+        case typeEnterprise::MASS: qDebug() << "Это предприятие имеет тип: MASS\n";  return type; break;
+    }
+
+}
+float getTax() override
+{
+    tax = getIncome() * 0.4 / getNumberOfWorkers();
+    return tax;
+}
+};
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Serial :public Enterprise
+{
+private:
+    typeEnterprise type =typeEnterprise::SERIAL;
+    float tax;
+
+public:
+    Serial()
+        :Enterprise()
+    {
+
+    }
+    Serial(QString companyName, double income, double square, int numberOfWorkers,QList<QString> lList)
+        :Enterprise(companyName, income, square, numberOfWorkers, lList)
+    {
+
+    }
+    Enterprise::typeEnterprise getType()  override
+    {
+        switch(type)
+        {
+            case typeEnterprise::SINGLE  : qDebug() << "Это предприятие имеет тип: SINGLE\n";  return type;  break;
+            case typeEnterprise::SERIAL  : qDebug() << "Это предприятие имеет тип: SERIAL\n";  return type; break;
+            case typeEnterprise::MASS: qDebug() << "Это предприятие имеет тип: MASS\n";  return type; break;
+        }
+    }
+    float getTax() override
+    {
+        tax = getIncome() * 0.1 / (getNumberOfWorkers() + getSquare());
+        return tax;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Mass :public Enterprise
+{
+private:
+    typeEnterprise type =typeEnterprise::MASS;
+    float tax;
+
+public:
+    Mass()
+        :Enterprise()
+    {
+
+    }
+    Mass(QString companyName, double income, double square, int numberOfWorkers,QList<QString> lList)
+        :Enterprise(companyName, income, square, numberOfWorkers, lList)
+    {
+
+    }
+
+    Enterprise::typeEnterprise getType()  override
+    {
+        switch(type)
+        {
+            case typeEnterprise::SINGLE  : qDebug() << "Это предприятие имеет тип: SINGLE\n";  return type;  break;
+            case typeEnterprise::SERIAL  : qDebug() << "Это предприятие имеет тип: SERIAL\n";  return type; break;
+            case typeEnterprise::MASS: qDebug() << "Это предприятие имеет тип: MASS\n";  return type; break;
+        }
+    }
+
+    float getTax() override
+    {
+        tax = getIncome() * 0.4 / (getNumberOfWorkers() + getSquare());
+        return tax;
+    }
+
+};
